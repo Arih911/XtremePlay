17eda6dde8d82378e3897fd0b582aa05ad62e4b9
Arihant Kamde <kamdearihant01@gmail.com>
2023-07-20 09:55:48 +0530

    try cath block added

--- app.js
+++ app.js
@@ -28,72 +28,105 @@ db.once("open", function () {
 
 
 app.get("/", async (req, res) => {
-    const movieList = await Movie.find({});
+    try {
+        const movieList = await Movie.find({});
+        res.render("home.ejs", {movieList});
+    } catch (error) {
+        console.log(error);
+    }
     // console.log(movieList)
-    res.render("home.ejs", {movieList});
 });
 
 app.post("/watchShow",  async (req, res) => {
-    const rawData = req.body.searchedMovie.toString().toLowerCase();
-    let movieArray = rawData.split(" ");
-    let movieName = "";
-    for(let part of movieArray){
-        movieName += `${part.charAt(0).toUpperCase()}${part.substring(1)} `;
+    try {
+        const rawData = req.body.searchedMovie.toString().toLowerCase();
+        let movieArray = rawData.split(" ");
+        let movieName = "";
+        for(let part of movieArray){
+            movieName += `${part.charAt(0).toUpperCase()}${part.substring(1)} `;
+        }
+        movieName = movieName.trimEnd();
+        // console.log(movieName)
+        const results = await Movie.find({name : movieName});
+        res.render("resultPage", {results, movieName});
+    } catch (error) {
+        console.log(error);
     }
-    movieName = movieName.trimEnd();
-    // console.log(movieName)
-    const results = await Movie.find({name : movieName});
-    res.render("resultPage", {results, movieName});
 });
 
 
 app.get("/watchShow/:movieName/:id", async(req, res)=>{
-    const {id, movieName} = req.params;
-    const movie = await Movie.findById(id);
-    
-    res.render("player", {movie, movieName});
+    try {
+        
+        const {id, movieName} = req.params;
+        const movie = await Movie.findById(id);
+        
+        res.render("player", {movie, movieName});
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 app.get("/goBack/:name/:movieName", async (req, res)=>{
-    const {name, movieName} = req.params;
-    const results = await Movie.find({name:name});
-    res.render("resultPage", {results, movieName});
+    try {
+        const results = await Movie.find({name:name});
+        res.render("resultPage", {results, movieName});
+        const {name, movieName} = req.params;
+        
+    } catch (error) {
+        console.log(error);
+    }
+        
 });
 
 
-app.get("/create", async (req, res)=>{
+app.get("/create",  (req, res)=>{
     res.render("new.ejs");
 });
 
 app.get("/delete", async (req, res)=>{
-    const movieList = await Movie.find({});
-    res.render("delete.ejs", {movieList});
+    try {
+        
+        const movieList = await Movie.find({});
+        res.render("delete.ejs", {movieList});
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 
 app.get("/deleteShow/:id", async (req, res)=>{
-    const {id} = req.params;
-    await Movie.findByIdAndDelete({_id:id});
-    const movieList = await Movie.find({});
-    // console.log(id);
-    res.render("delete", {movieList});
+   try {
+       const {id} = req.params;
+       await Movie.findByIdAndDelete({_id:id});
+       const movieList = await Movie.find({});
+       // console.log(id);
+       res.render("delete", {movieList});
+    
+   } catch (error) {
+     console.log(error);
+   }
 })
 
 
 
 app.post("/create", async (req, res)=>{
-    const {name, url} = req.body;
-    let description;
-    if(req.body.description){
-        description = req.body.description;
+    try {
+        const {name, url} = req.body;
+        let description;
+        if(req.body.description){
+            description = req.body.description;
+        }
+        const movie = await new Movie({name: name ,url : url});
+        if(description){
+            movie.description = description;
+        }
+        await movie.save();
+        res.redirect("create");
+        
+    } catch (error) {
+        console.log(error);
     }
-    const movie = await new Movie({name: name ,url : url});
-    if(description){
-        movie.description = description;
-    }
-    await movie.save();
-    console.log("success")
-    res.redirect("create");
 });
 
 app.listen(3000, (req, res) => {
