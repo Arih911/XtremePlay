deced0d0f46c64b8a0721f0748bb79fbf73e401f
arihantXD <kamdearihant01@gmail.com>
2023-09-14 15:34:30 +0530

    hosting changed

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const Movie = require("./models/movie");
 
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+app.set(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 const dbUrl = process.env.DB_URL;
